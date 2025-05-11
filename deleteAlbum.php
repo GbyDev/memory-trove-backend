54a4ef0440a69214cbee134c37803000d7b117ca
GbyDev <kamimamita8kuji@gmail.com>
2025-05-11 21:43:33 +0800

    Fix album deletion functionality in deleteAlbum.php by updating variable names and improving message handling

--- deleteAlbum.php
+++ deleteAlbum.php
@@ -11,14 +11,47 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 }
 
 $data = json_decode(file_get_contents('php://input'), true);
-$album_id = $data['albumId'];
-$album_folder_path = $data['albumFolderPath'];
+$album_id = $data['album_id'];
+$album_folder_path = $data['album_folder_path'];
 
+$message = "";
+$messageType = "";
+
+function setMessage($msgType, $msg) {
+    global $messageType, $message;
+    $messageType = $msgType;
+    $message = $msg;
+}
 
 function delete_album_from_database(){
     global $conn, $album_id;
-    $sql = "DELETE FROM album WHERE album_id = $album_id";
+    $sql = "DELETE FROM albums WHERE album_id = $album_id";
     $conn->query($sql);
+}
+
+function delete_directory($dir) {
+    if (!file_exists($dir)) {
+        return false; // Folder does not exist
+    }
+
+    if (!is_dir($dir)) {
+        return unlink($dir); // Just a file
+    }
+
+    foreach (scandir($dir) as $item) {
+        if ($item === '.' || $item === '..') {
+            continue;
+        }
+
+        $path = $dir . DIRECTORY_SEPARATOR . $item;
+        if (is_dir($path)) {
+            delete_directory($path);
+        } else {
+            unlink($path);
+        }
+    }
+
+    return rmdir($dir);
 }
 
 function delete_album_folder(){
@@ -28,9 +61,10 @@ function delete_album_folder(){
 
 delete_album_from_database();
 delete_album_folder();
+setMessage("black", "album id: $album_id album folder path: $album_folder_path");
 
 exit(json_encode([
-    "message" => "Album deleted successfully.",
-    "messageType" => "success"
+    "message" => $message,
+    "messageType" => $messageType,
 ]));
 ?>
